24d7c911caef7746e17ad24fcad993a0882b3b33
Alex Collins <alex.e.c@gmail.com>
2013-01-05 11:22:27 +0000

    Bug: a couple of bugs with defn templates

--- vbox-core/src/main/vbox/CentOS_6_3/floppy0/post-install.sh
+++ vbox-core/src/main/vbox/CentOS_6_3/floppy0/post-install.sh
@@ -1,9 +1,6 @@
 #! /bin/sh
 set -ue
 
-ifup eth0
-sed -i 's/ONBOOT="no"/ONBOOT="yes"/' /etc/sysconfig/network-scripts/ifcfg-eth0
-
 yum -y install kernel-headers kernel-devel gcc gcc-c++ make kernel-devel-$(uname -r)
 mkdir /media/cdrom
 mount /dev/sr0 /media/cdrom
