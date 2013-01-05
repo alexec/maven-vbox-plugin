7022cb51ae1538efb56b09cc3e4f673dc055771d
Alex Collins <alex.e.c@gmail.com>
2013-01-05 11:21:37 +0000

    Bug: wrong vis on field

--- vbox-core/src/test/java/com/alexecollins/vbox/core/task/AbstractTest.java
+++ vbox-core/src/test/java/com/alexecollins/vbox/core/task/AbstractTest.java
@@ -2,8 +2,12 @@ package com.alexecollins.vbox.core.task;
 
 import com.alexecollins.vbox.core.VBox;
 import org.junit.runners.Parameterized;
+import org.xml.sax.SAXException;
 
+import javax.xml.bind.JAXBException;
 import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.Collection;
 
@@ -15,19 +19,17 @@ public abstract class AbstractTest {
 	@Parameterized.Parameters
 	public static Collection<Object[]> data() {
 		return Arrays.asList(new Object[][]{
-				{"CentOS_6_3"} //,
-				//{"UbuntuServer_12_10" },
-				//{"WindowsServer2008" }
+				{"UbuntuServer_12_10" },{"CentOS_6_3"},{"WindowsServer2008" }
 		});
 	}
 
 	protected final String name;
-	final File src;
-	final VBox box;
+	protected final File src;
+	protected final VBox box;
 	final File target;
 	final File work = new File("target");
 
-	public AbstractTest(final String name) {
+	public AbstractTest(final String name) throws IOException, JAXBException, SAXException, URISyntaxException {
 		this.name = name;
 		System.out.println("name=" + name);
 		src = new File("src/main/vbox/" + name);
