d07c1f3767273d8edc60560363274bed8e3811a3
Alex Collins <alex.e.c@gmail.com>
2013-02-24 21:54:07 +0000

    Complete Ant part.

---
+++ vbox-ant-tasks/src/main/java/com/alexecollins/vbox/ant/DeleteDefinitionTask.java
@@ -0,0 +1,26 @@
+package com.alexecollins.vbox.ant;
+
+import com.alexecollins.vbox.core.VBox;
+import com.alexecollins.vbox.core.task.DeleteDefinition;
+import org.apache.tools.ant.BuildException;
+
+/**
+ * Delete a definition, along with it's box.
+ *
+ * @author alexec (alex.e.c@gmail.com)
+ * @since 3.0.0
+ */
+public class DeleteDefinitionTask extends AbstractTask {
+
+	@Override
+	public void execute() throws BuildException {
+		if (dir == null) {
+			throw new BuildException("dir is null");
+		}
+		try {
+			new DeleteDefinition(work(), new VBox(context(), dir.toURI())).call();
+		} catch (Exception e) {
+			throw new BuildException(e);
+		}
+	}
+}
