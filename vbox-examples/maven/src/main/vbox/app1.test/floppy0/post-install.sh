c2b7d611d88c201e15371ec6d1786957dc1e9197
Alex Collins <alex.e.c@gmail.com>
2013-04-13 18:50:11 +0100

    added file

---
+++ vbox-examples/maven/src/main/vbox/app1.test/floppy0/post-install.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+set -ue
+
+yum -y install acpid kernel-headers kernel-devel gcc gcc-c++ make kernel-devel-$(uname -r)
+mkdir /media/cdrom
+mount /dev/sr0 /media/cdrom
+/media/cdrom/VBoxLinuxAdditions.run
+umount /dev/sr0
+rm -R /media/cdrom
+
+yum -y install tomcat6
+chkconfig tomcat6 on
+
+poweroff now
